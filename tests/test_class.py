1805b8642a5dbb8f2a944c14fcf90e5a162cad36
unknown <getjames20@gmail.com>
2018-06-28 19:22:59 +0300

    Modify testfile

--- tests/test_class.py
+++ tests/test_class.py
@@ -2,7 +2,7 @@ import unittest
 import json
 import sys,os
 sys.path.append(os.path.pardir)
-from api.run import APP
+from run import APP
 
 class TestClass(unittest.TestCase):
     def setUp(self):
